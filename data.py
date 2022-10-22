5e2377ac11be7aaca14d9211d5a840ecee249ea3
SarthakJha-IITMandi <jhasarthak123@gmail.com>
2022-10-22 19:20:21 +0530

    added dummy-files and routes to articles

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Article One",
+            "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            "author": "Sarthak Jha",
+            "create_Date": "22-10-2022",
+        },
+        {
+            "id": 2,
+            "title": "Article Two",
+            "body": "Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook",
+            "author": "Dummy-User2",
+            "create_Date": "23-10-2022",
+        },
+        {
+            "id": 3,
+            "title": "Article Three",
+            "body": "Het is al geruime tijd een bekend gegeven dat een lezer, tijdens het bekijken van de layout van een pagina, afgeleid wordt door de tekstuele inhoud. Het belangrijke punt van het gebruik van Lorem Ipsum is dat het uit een min of meer normale verdeling van letters bestaat, in tegenstelling tot Hier uw tekst, hier uw tekst wat het tot min of meer leesbaar",
+            "author": "Dummy-User3",
+            "create_Date": "22-10-2022",
+        },
+    ]
+
+    return articles
